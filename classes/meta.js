cfdce7021767425a6e933198480c5796b27b1f6a
royossai <royossai@yahoo.com>
2021-03-17 10:56:19 +0100

    spring update, prep homebase

--- classes/meta.js
+++ classes/meta.js
@@ -33,7 +33,7 @@ class Client {
     async logIp(addr, request) {
         let country, region, city, isp, alias;
         let client = await Entry.findOne({where: {request, addr}});
-        if (!addr.includes("127.0.0.1") && !request.includes('load/') && !addr.includes('192.168')) {
+        if (addr !== '::1' && !addr.includes("127.0.0.1") && !request.includes('load/') && !addr.includes('192.168')) {
             if (client === null) {
                 client = await Entry.findOne({where: {addr}});
                 if (client) {
