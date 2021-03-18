f080936ad6f9c86f5c0347ebcfd90d47574b6b22
royossai <royossai@yahoo.com>
2021-03-18 21:36:30 +0100

    fixed package.json

--- server/public/scripts/editor.js
+++ server/public/scripts/editor.js
@@ -102,7 +102,8 @@ submit.onclick = async () => {
     let selected = [{index: 0, name: select_one.value}, {index: 1, name: select_two.value}];
     if (name === "" && document.querySelectorAll('.remove').length) alert("Please enter a category");
     if (display === "" && document.querySelectorAll('.remove').length) alert("Please enter display info");
-    if ((select_one.value === select_two.value) && select_two.value !== '') alert('both editor picks cannot have the same value');
+    if ((select_one.value === select_two.value) && (select_two.value !== '')) alert('both editor picks cannot have the same value');
+    if ((select_one.value !== '' || select_two.value !== '') && (select_one.value !== select_two.value)) alert('either both picks have a value or none do');
     else {
         let blob = [];
         document.querySelectorAll('.remove').forEach(div => {
