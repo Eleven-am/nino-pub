d055ee29e2eda1bf31db9db25536ffb8d8033892
royossai <royossai@yahoo.com>
2021-04-05 02:17:14 +0200

    unification of nino's public and private repos

--- server/app.js
+++ server/app.js
@@ -40,6 +40,6 @@ const port = process.env.PORT || 5000;
         })
     }
 
-    app.listen(port,() => console.log("listening on port: http://localhost:" + 5000));
+    app.listen(port,() => console.log("listening on port: " + 5000));
 })()
 
